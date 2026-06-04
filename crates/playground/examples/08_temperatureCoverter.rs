88adbcce9c23a18d51f5b4a49786b4cb2a678ebd
zouyugangIan <zyg1214@yeah.net>
2026-06-04 22:58:14 +0800

    feat:improve the examples of code in rustbook, it's still in work.

--- crates/playground/examples/08_temperatureCoverter.rs
+++ crates/playground/examples/08_temperatureCoverter.rs
@@ -8,14 +8,15 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line");
         covertFtoS();
-    }
-    let mut x = 5;
+    
+    let mut x = 5;      
     let y = {
         let x = 3;
         x + 1
     };
     println!("The value of y is: {y}");
     println!("The value of x is: {x}");
+    }
 }
 
 fn covertFtoS() {
---
+++ crates/playground/examples/11_collection.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut vec = vec![1, 2, 3, 4, 5];
+
+    for i in 0..vec.len() {
+        println!("{ }", &vec[i]);
+        vec[i] += 1;
+        vec.push(vec[i + 1]);
+        println!("{}", vec.last().unwrap());
+    }
+    for element in vec.iter() {
+        println!("the value is:{}", element);
+    }
+}
