887492565f06ae4aca3e7ddab42bc2b214f30f1b
zouyugangIan <zyg1214@yeah.net>
2026-06-11 21:56:00 +0800

    feat:改进示例

--- crates/playground/examples/11_collection.rs
+++ crates/playground/examples/11_collection.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 fn main() {
     let mut vec = vec![1, 2, 3, 4, 5];
 
     for i in 0..vec.len() {
-        println!("{ }", &vec[i]);
+        println!("{}", &vec[i]);
         vec[i] += 1;
         vec.push(vec[i + 1]);
         println!("{}", vec.last().unwrap());
@@ -10,5 +11,11 @@ fn main() {
     for element in vec.iter() {
         println!("the value is:{}", element);
         println!("{}", element);
+    }
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 33);
+    for (key, value) in &scores {
+        println!("key is {},score is{}", key, value);
     }
 }
