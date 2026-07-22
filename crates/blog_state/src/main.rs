ba71cabfe236166d7b792c9c677bdbb8a108d8c4
zouyugangIan <zyg1214@yeah.net>
2026-07-23 00:39:42 +0800

    feat:改进案例

---
+++ crates/blog_state/src/main.rs
@@ -0,0 +1,17 @@
+use blog_state::*;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("This morning, I got up early and learned rust for a while!");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!(
+        "This morning, I got up early and learned rust for a while!",
+        post.content()
+    );
+}
