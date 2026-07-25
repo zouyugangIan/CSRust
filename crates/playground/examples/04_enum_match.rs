ca7fe7b64ea93bdd30b32c76a15b7e7b7820854a
zouyugangIan <zyg1214@yeah.net>
2026-07-25 16:33:45 +0800

    fix:改进enum_match示例

--- crates/playground/examples/04_enum_match.rs
+++ crates/playground/examples/04_enum_match.rs
@@ -1,3 +1,39 @@
 fn main() {
+    let language = Language::English;
+    match language {
+        Language::English => println!("Hello!"),
+        Language::Russian => println!("Привет!"),
+        Language::Chinese => println!("你好!"),
+        Language::Spanish => println!("Hola!"),
+        Language::French => println!("Bonjour!"),
+        Language::German => println!("Hallo!"),
+        Language::Japanese => println!("こんにちは!"),
+        Language::Portuguese => println!("Olá!"),
+        _ => println!("Unknown language!"),
+    }
+}
 
+impl Language {
+    fn getLanguage(&self) -> &str {
+        match self {
+            Language::English => "English",
+            Language::Russian => "Russian",
+            Language::Chinese => "Chinese",
+            Language::Spanish => "Spanish",
+            Language::French => "French",
+            Language::German => "German",
+            Language::Japanese => "Japanese",
+            Language::Portuguese => "Portuguese",
+        }
+    }
+}
+enum Language {
+    English,
+    Russian,
+    Chinese,
+    Spanish,
+    French,
+    German,
+    Japanese,
+    Portuguese,
 }
