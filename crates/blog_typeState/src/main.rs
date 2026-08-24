ba4e202bf48e75a0f2c2bee84bb6809a4b1ffc90
zyg <zyg1214@yeah.net>
2026-08-24 16:10:56 +0800

    feat:complete the example code of the Hello Rust html

--- crates/blog_typeState/src/main.rs
+++ crates/blog_typeState/src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use blog_state::*;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("This morning, I got up early and learned rust for a while!");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!(
+        "This morning, I got up early and learned rust for a while!",
+        post.content()
+    );
+    println!("Content:{}", post.content());
+}
